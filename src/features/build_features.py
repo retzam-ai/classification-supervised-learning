83fd11be604214913b425ab64f4eaacfc33bbf36
retzam <dretzam@gmail.com>
2024-05-02 12:25:03 +0100

    Update build features add machines data

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,4 +1,6 @@
 from src.data.processed.cars_data import PrepocessedCarsData
+from src.data.processed.injury_data import PrepocessedInjuryData
+from src.data.processed.machine_data import PrepocessedMachineData
 class BuildFeatures:
     
     def __init__(self, dataset):
@@ -7,6 +9,12 @@ class BuildFeatures:
     def get_data(self, dataset_type, oversample=False):
         if self.dataset == 'cars':
             prepocessed_data = PrepocessedCarsData()
+        
+        if self.dataset == 'injury':
+            prepocessed_data = PrepocessedInjuryData()
+            
+        if self.dataset == 'machines':
+            prepocessed_data = PrepocessedMachineData()
             
         if dataset_type == 'test':
             test, X_test, y_test = prepocessed_data.get_data(dataset_type, oversample)
