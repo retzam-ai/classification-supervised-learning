73e8a811b5d3c8bd143c1eb5799fb64c527def23
retzam <dretzam@gmail.com>
2024-05-02 12:23:53 +0100

    Update train model add logistic regression

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,18 +1,35 @@
 import argparse
+from src.models.constants import DATASETS, MODELS
 from src.models.train.knn import TrainKNNModel
+from src.models.train.logistic_regression import TrainLogisticRegressionModel
 from src.models.train.naive_bayes import TrainNaiveBayesModel
 
 def main(args):
-    print('args: ', args)
-    if args.model == 'knn':
+    model_name = args.model
+    dataset_name = args.dataset
+    
+    print("dataset_name: ", dataset_name)
+    print("datasets: ", DATASETS)
+    
+    if model_name not in MODELS:
+        return print(f"Unsupported model: {model_name}")
+    if  dataset_name not in DATASETS:
+        return print(f"Unsupported dataset: {dataset_name}")
+    
+    if model_name == 'knn':
         model = TrainKNNModel(args.dataset)
         model.train()
         return print(f"KNN model successfully trained with {args.dataset}.")
 
-    if args.model == 'naive_bayes':
+    if model_name == 'naive_bayes':
         model = TrainNaiveBayesModel(args.dataset)
         model.train()
         return print(f"Naive Bayes model successfully trained with {args.dataset}.")
+    
+    if model_name == 'logistic_regression':
+        model = TrainLogisticRegressionModel(args.dataset)
+        model.train()
+        return print(f"Logistic Regression model successfully trained with {args.dataset}.")
     
     print("No model specificied for training.")
 
