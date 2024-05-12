16d6ee1901f1edb8dd90de10a23fe64e7ef1df03
retzam <dretzam@gmail.com>
2024-05-12 20:41:21 +0100

    Add random forest model

---
+++ src/models/train/random_forest.py
@@ -0,0 +1,18 @@
+from sklearn.ensemble import RandomForestClassifier
+from src.features.build_features import BuildFeatures
+from joblib import dump
+
+class TrainRandomForestModel:
+    
+    def __init__(self, dataset):
+        self.model = RandomForestClassifier()
+        self.prepocessed_data = BuildFeatures(dataset)
+        self.dataset = dataset
+        
+    def train(self):
+        train, X_train, y_train = self.prepocessed_data.get_data('train', oversample=True)
+        self.model.fit(X_train, y_train)
+        
+        # save the trained model
+        model_name = f"models/{self.dataset}/{self.dataset}-random-forest-model.joblib"
+        dump(self.model, model_name)
