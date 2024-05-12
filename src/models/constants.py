4ebc75363b34a80e3fb5643c0f152da048d35afa
retzam <dretzam@gmail.com>
2024-05-12 20:39:18 +0100

    Update constants

--- src/models/constants.py
+++ src/models/constants.py
@@ -1,2 +1,2 @@
-MODELS = ['knn', 'naive_bayes', 'logistic_regression']
-DATASETS = ['cars', 'injury', 'machines']
+MODELS = ['knn', 'naive_bayes', 'logistic_regression', 'svm' ,'random_forest']
+DATASETS = ['cars', 'injury', 'machines', 'marketing', 'diabetes']
